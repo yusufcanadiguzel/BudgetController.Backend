164c60ead850e8134385abdd8d936fc7f6057c9d
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2023-03-10 07:48:20 +0300

    Entities Created
    Database tables are created.

---
+++ Entities/Concrete/Receipt.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Receipt : IEntity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int CompanyId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
